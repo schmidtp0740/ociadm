6fe9908ed26140bf77d758b9e2691a431ddeeabd
schmidtp0740@ <schmidtp0740@gmail.com>
2018-05-02 19:06:26 -0500

    added createVCN function

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/oracle/oci-go-sdk/common"
+	"github.com/oracle/oci-go-sdk/core"
+)
+
+func main() {
+	config := common.DefaultConfigProvider()
+	var cidrBlock, compartmentID, vcnDisplayName, dnsLabel string
+
+	createVCN(config, cidrBlock, compartmentID, vcnDisplayName, dnsLabel)
+}
+
+func createVCN(config common.ConfigurationProvider, CidrBlock, CompartmentID, vcnDisplayName, dnsLabel string) core.Vcn {
+	c, err := core.NewVirtualNetworkClientWithConfigurationProvider(config)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	request := core.CreateVcnRequest{}
+	request.CidrBlock = common.String(CidrBlock)
+	request.CompartmentId = common.String(CompartmentID)
+	request.DisplayName = common.String(vcnDisplayName)
+	request.DnsLabel = common.String(dnsLabel)
+
+	r, err := c.CreateVcn(ctx, request)
+	if err != nil {
+		panic(err)
+	}
+
+	return r.Vcn
 }
