d43c29842c5532d79604186adf1f17898f1a8ddf
schmidtp0740 <schmidtp0740@gmail.com>
2019-02-15 14:39:32 -0600

    removed unnecessary print

--- cmd/services/cost/cost_test.go
+++ cmd/services/cost/cost_test.go
@@ -1,14 +1,12 @@
 package cost
 
 import (
-	"fmt"
 	"log"
 	"testing"
 )
 
 // TestCost ...
 func TestCost(t *testing.T) {
-	fmt.Print("test")
 	err := Cmd.Execute()
 	if err != nil {
 		log.Fatal(err.Error())
